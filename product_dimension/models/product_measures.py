8a1758d3c41f6499f8332bf8d2d6a3098ee187ba
RedesSocialesJLBB <redes_sociales@jlbberp.com>
2024-12-19 09:30:34 +0100

    Update product_measures.py

--- product_dimension/models/product_measures.py
+++ product_dimension/models/product_measures.py
@@ -8,20 +8,29 @@ class ProductMeasures(models.Model):
     product_measures_length = fields.Many2one(
         "product.measures",
         "Largo",
-        readonly=False
+        readonly=False,
+        store=True
     )
     product_measures_width = fields.Many2one(
         "product.measures",
         "Ancho",
-        readonly=False
+        readonly=False,
+        store=True
     )
     product_measures_thickness = fields.Many2one(
         "product.measures",
         "Grueso",
+        readonly=False,
+        store=True
+    )
+    product_template_id = fields.One2many(
+        "product.template",
+        "product_measures_id",
+        "ID de Producto",
         readonly=False
     )
-    product_template_id = fields.Many2one(
-        "product.template",
-        "ID de Producto",
-        readonly=True
+    product_category_id = fields.Many2one(
+        "product.category",
+        "ID de categoría del producto",
+        readonly=False
     )
